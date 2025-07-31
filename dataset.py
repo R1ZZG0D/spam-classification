af3c0eff79f9cb34af1e9fd54c90d9d1b69c1216
R1ZZG0D <xxsirdeadshotxx@gmail.com>
2025-07-31 19:25:56 -0400

    first commit

---
+++ dataset.py
@@ -0,0 +1,23 @@
+import requests
+import zipfile
+import io
+import os
+
+'''
+# URL of the dataset
+url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
+# Download the dataset
+response = requests.get(url)
+if response.status_code == 200:
+    print("Download successful")
+else:
+    print("Failed to download the dataset")
+
+# Extract the dataset
+with zipfile.ZipFile(io.BytesIO(response.content)) as z:
+    z.extractall("sms_spam_collection")
+    print("Extraction successful")
+'''
+# List the extracted files
+extracted_files = os.listdir("sms_spam_collection")
+print("Extracted files:", extracted_files)
